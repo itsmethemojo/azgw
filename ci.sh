5e043d92eb8ff14b37bfdb88b4e0e6244bde5ff4
Marian Pöschmann <github@mail.itsmethemojo.eu>
2020-04-01 23:15:32 +0200

    add automatic bash linting, add automatic javascript linting, add ci.sh fix-js command for auto fixing codestyle

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ case "$1" in
   if [ ! -d tests/bats ];
   then
     echo "INFO downloading bats test framework"
-    docker run -v "$(pwd)":/app -w /app buildpack-deps:stretch bash -c "
+    docker run --rm -v "$(pwd)":/app -w /app buildpack-deps:stretch bash -c "
     git clone --depth 1 https://github.com/sstephenson/bats.git /app/tests/bats &> /dev/null &&
     rm -r /app/tests/bats/.git &&
     chmod -R 777 /app/tests/bats
@@ -16,8 +16,45 @@ case "$1" in
   tests/bats/bin/bats tests/tests.bats
   exit $?
   ;;
+"lint-bash")
+  if [ ! -d tests/shellcheck-stable ];
+  then
+    echo "INFO downloading shellcheck"
+    docker run --rm -v "$(pwd)":/app -w /app buildpack-deps:stretch bash -c "
+    cd /app/tests &&
+    wget -qO- 'https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz' | tar -xJ &&
+    chmod -R 777 /app/tests/shellcheck-stable
+    "
+  else
+    echo "INFO skip downloading shellcheck"
+  fi
+  tests/shellcheck-stable/shellcheck ./*.sh
+  exit $?
+  ;;
+"lint-js")
+  if [ ! -d node_modules ];
+  then
+    echo "INFO npm install"
+    docker run --rm --entrypoint npm -w /app -v "$(pwd)":/app node install
+  else
+    echo "INFO skip npm install"
+  fi
+  docker run --rm -w /app -v "$(pwd)":/app cytopia/eslint src/*.js
+  exit $?
+  ;;
+"fix-js")
+  if [ ! -d node_modules ];
+  then
+    echo "INFO npm install"
+    docker run --rm --entrypoint npm -w /app -v "$(pwd)":/app node install
+  else
+    echo "INFO skip npm install"
+  fi
+  docker run --rm -w /app -v "$(pwd)":/app cytopia/eslint --fix src/*.js
+  exit $?
+  ;;
 "clear")
-  rm -rf tests/bats tests/debug
+  rm -rf tests/bats tests/debug tests/shellcheck-stable node_modules
   exit 0
   ;;
 esac
@@ -26,6 +63,10 @@ HELP_TEXT="Usage: ci [COMMAND]
 
 Commands:
   test                      runs test suite with bats
+  lint-bash                 lints bash scripts
+  lint-js                   lints javascript files
+  fix-js                    lints and auto-fixes javascript files
+  clear                     removes temporary artefacts
 "
 echo -e "${HELP_TEXT}"
 exit 0
--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
-const puppeteer = require('puppeteer');
-const url = process.env.GANGWAY_URL;
-const email = process.env.AZURE_EMAIL;
-const password =process.env.AZURE_PASSWORD;
-
-(async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
-  const page = await browser.newPage();
-  await page.goto(url + '/login');
-  const azureLoginIcon = '.dex-btn-icon.dex-btn-icon--microsoft';
-  await page.waitForSelector(azureLoginIcon);
-  await page.click(azureLoginIcon);
-  await page.waitForNavigation({waitUntil: 'networkidle2'});
-  await page.waitForSelector('#i0116');
-  const emailInputField = await page.$('#i0116');
-  await emailInputField.type(email);
-  await emailInputField.press('Enter');
-  await page.waitFor(2000);
-  await page.waitForSelector('input[type=password]');
-  const bodyHandle = await page.$('body');
-  const html = await page.evaluate(body => body.innerHTML, bodyHandle);
-  const passwordInputField = await page.$('input[type=password]');
-  await passwordInputField.type(password);
-  await passwordInputField.press('Enter');
-  await page.waitForNavigation({waitUntil: 'networkidle2'});
-  const codeBoxesSelector = 'div.code-toolbar';
-  const codeBoxesDom = await page.$(codeBoxesSelector);
+const puppeteer = require('puppeteer')
+const url = process.env.GANGWAY_URL
+const email = process.env.AZURE_EMAIL
+const password = process.env.AZURE_PASSWORD
+// eslint-disable-next-line no-unused-vars
+const login = (async () => {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+  const page = await browser.newPage()
+  await page.goto(url + '/login')
+  const azureLoginIcon = '.dex-btn-icon.dex-btn-icon--microsoft'
+  await page.waitForSelector(azureLoginIcon)
+  await page.click(azureLoginIcon)
+  await page.waitForNavigation({ waitUntil: 'networkidle2' })
+  await page.waitForSelector('#i0116')
+  const emailInputField = await page.$('#i0116')
+  await emailInputField.type(email)
+  await emailInputField.press('Enter')
+  await page.waitFor(2000)
+  await page.waitForSelector('input[type=password]')
+  const passwordInputField = await page.$('input[type=password]')
+  await passwordInputField.type(password)
+  await passwordInputField.press('Enter')
+  await page.waitForNavigation({ waitUntil: 'networkidle2' })
+  const codeBoxesSelector = 'div.code-toolbar'
   const codeBoxes = await page.evaluate((selector) => {
-    const codeBoxesNodeList = document.querySelectorAll(selector);
-    const anchors = [...codeBoxesNodeList];
-    return anchors.map(element => element.innerHTML);
-  }, codeBoxesSelector);
-  console.log(codeBoxes[1].replace(/<\/?[^>]+(>|$)/g, "").replace(/   Copy/g, "").replace(/&gt;/,'>'));
-  await browser.close();
-})();
+    const codeBoxesNodeList = document.querySelectorAll(selector)
+    const anchors = [...codeBoxesNodeList]
+    return anchors.map(element => element.innerHTML)
+  }, codeBoxesSelector)
+  console.log(codeBoxes[1].replace(/<\/?[^>]+(>|$)/g, '').replace(/ {3}Copy/g, '').replace(/&gt;/, '>'))
+  await browser.close()
+})()
